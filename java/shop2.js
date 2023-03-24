311566b76ab1d1cde34c2fef053ec48617081be7
Juskoh Afonso <juskoh_96@hotmail.com>
2023-03-24 21:29:56 +0000

    Shop1 Shop2 Shop3 Produtos foram add no javascript, array tambem foram add, todas as imagens foram transferidas, estamos pronto para comecar

---
+++ java/shop2.js
@@ -0,0 +1,90 @@
+const shop2 = [
+  {
+    id: 0,
+    name: "Antonella Ring",
+    base: "silver",
+    stone: "emerald",
+    befor: 11000,
+    price: 5500,
+    acessory: "ring",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+  {
+    id: 1,
+    name: "Berceste Earrings",
+    base: "silver",
+    stone: "safira",
+    befor: 15050,
+    price: 7525,
+    acessory: "earrings",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+  {
+    id: 2,
+    name: "Eberta Pingent",
+    base: "silver",
+    stone: "diamond",
+    befor: 9500,
+    price: 4750,
+    acessory: "pingent",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+  {
+    id: 3,
+    name: "Ellen Bracelet",
+    base: "gold",
+    stone: "diamond",
+    befor: 5000,
+    price: 2500,
+    acessory: "bracelet",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+  {
+    id: 4,
+    name: "Badhild Bracelet",
+    base: "silver",
+    stone: "emerald",
+    befor: 7600,
+    price: 3800,
+    acessory: "bracelet",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+  {
+    id: 5,
+    name: "Arlenys Pingent",
+    base: "gold",
+    stone: "emerald",
+    befor: 17000,
+    price: 8500,
+    acessory: "pingent",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+  {
+    id: 6,
+    name: "Eva Ring",
+    base: "gold",
+    stone: "ruby",
+    befor: 22000,
+    price: 11000,
+    acessory: "ring",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+  {
+    id: 7,
+    name: "Dajana Earrings",
+    base: "gold",
+    stone: "diamond",
+    befor: 13300,
+    price: 6650,
+    acessory: "earrings",
+    descript:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, nobis consectetur labore sint, quod repudiandae nihil iusto reiciendis enim facilis iure qui cum doloremque non tempore expedita. Odio, perspiciatis fugit!",
+  },
+];
